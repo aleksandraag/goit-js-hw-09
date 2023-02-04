53781b1c719c5514cac8bb10741c02884a11f4c1
aleksandraag <ola.golinska@vp.pl>
2023-02-05 00:35:43 +0100

    task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+const getRandomHexColor = () => {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
 
+let interval;
+
+const btnStart = document.querySelector('[data-start]');
+const btnStop = document.querySelector('[data-stop]');
+
+btnStop.setAttribute('disabled', '');
+
+btnStart.addEventListener('click', () => {
+  interval = setInterval(
+    () => (document.body.style.backgroundColor = getRandomHexColor()),
+    1000
+  );
+  btnStart.setAttribute('disabled', '');
+  btnStop.removeAttribute('disabled');
+});
+
+btnStop.addEventListener('click', () => {
+  clearInterval(interval);
+  btnStart.removeAttribute('disabled');
+  btnStop.setAttribute('disabled', '');
+});
